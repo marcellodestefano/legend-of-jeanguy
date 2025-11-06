3f8fbf00c2de39d2271c70f1f38f0666f1381183
marcellodestefano <marcello_destefano@hotmail.com>
2025-11-06 11:08:21 +0100

    :construction class Players

---
+++ src/main/java/com/LegendOfJG/entities/NonPlayable.java
@@ -0,0 +1,5 @@
+package main.java.com.LegendOfJG.entities;
+
+public class NonPlayable {
+
+}
--- src/main/java/com/LegendOfJG/entities/Players.java
+++ src/main/java/com/LegendOfJG/entities/Players.java
@@ -9,7 +9,86 @@ abstract class Players implements Actions{
     protected int range;
     protected int hp;
     protected float speed;
-    protected boolean isDead;
+    protected boolean isDead = false;
     protected boolean isMelee;
+    protected int attackSpeed;
+    protected boolean killable;
+    protected List<String> soundPaths = new ArrayList<>();
+    protected List<String> spritesPaths = new ArrayList<>();
+    static int id;
+
+
+    public Players(String name, int damage, List<Integer> position, int range, int hp, float speed, boolean isMelee, int attackSpeed, boolean killable, List<String> soundPaths, List<String> spritesPaths ) {
+        id = id++;
+        this.name = name;
+        this.damage = damage;
+        this.position = position;
+        this.range = range;
+        this.hp = hp;
+        this.speed = speed;
+        this.isMelee = isMelee;
+        this.attackSpeed = attackSpeed;
+        this.killable = killable;
+        this.soundPaths = soundPaths;
+        this.spritesPaths = spritesPaths;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public int getDamage() {
+        return this.damage;
+    }
+    public List<Integer> getPosition() {
+        return this.position;
+
+    }
+    public int getRange() {
+        return this.range;
+    }
+
+    public int getHp() {
+        return this.hp;
+    }
+
+    public float getSpeed() {
+        return this.speed;
+    }
+
+    public boolean isMelee() {
+        return this.isMelee;
+    }
+
+    public boolean isDead() {
+        return this.isDead;
+    }
+
+    public void killed(){
+        this.isDead = true;
+    }
+
+    public boolean isKillable() {
+        return this.killable;
+    }
+
+    public List<String> getSoundPaths() {
+        return this.soundPaths;
+    }
+
+    public List<String> getSpritesPaths() {
+        return this.spritesPaths;
+    }
+
+
+
+
+
+
+
 
 }
