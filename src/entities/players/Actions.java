d64e1326cb1dceab4113bc5f77e02b2d9d6a2197
marcellodestefano <marcello_destefano@hotmail.com>
2025-11-08 21:52:47 +0100

    comments

--- src/entities/players/Actions.java
+++ src/entities/players/Actions.java
@@ -3,6 +3,15 @@ import java.util.*;
 
 
 interface Actions{
+    /*
+    Méthodes communes à tous les joueurs
+
+    Cette interface définit les actions de base que tous les joueurs (joueurs jouables et non jouables) doivent implémenter.
+    Elle inclut des méthodes pour obtenir le nom, les points de vie, la position,
+    attaquer, et recevoir des dégâts.
+    On a aussi les méthods nous permettant de acceder aux ressources graphiques et sonores.
+    Les méthodes de déplacement vont probablement être supprimées ou modifiées plus tard.
+     */
     String getName();
     int getDamage();
     List<Integer> getPosition();
