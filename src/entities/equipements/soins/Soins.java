c0db4a7955bb7ec2210b5ac1f8448c96d1052809
marcellodestefano <marcello_destefano@hotmail.com>
2025-11-10 14:40:43 +0100

    :bug: super first line

--- src/entities/equipements/soins/Soins.java
+++ src/entities/equipements/soins/Soins.java
@@ -7,8 +7,9 @@ abstract class Soins extends Equipements{
     protected int dropPercentage;
 
     public Soins(boolean drop, String name, int unite, List<String> spritePath, int dropPercentage){
-        this.dropPercentage = dropPercentage;
         super(drop, name, unite, spritePath);
+        this.dropPercentage = dropPercentage;
+
     }
 
     public int getDropPercentage() {
