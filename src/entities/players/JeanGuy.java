0b5f2b7860430835acfa4c37cf1e6ad5b58c1260
Vansevertheo <theo.van-sever@epitech.eu>
2025-11-10 10:47:28 +0100

    :sparkles: Ajout module coeurmax

--- src/entities/players/JeanGuy.java
+++ src/entities/players/JeanGuy.java
@@ -1,11 +1,23 @@
 package entities.players;
 
 
+import entities.equipements.soins.CoeurMax;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class JeanGuy extends Playable {
+    protected int HpMax = 5;
+
     public JeanGuy(){
-        super("Jean-Guy", 0, new ArrayList<Integer>(Arrays.asList(7,7,0)), 2, 100,5,false,true,2,true, Arrays.asList("",""),Arrays.asList("",""));
+        super("Jean-Guy", 0, new ArrayList<Integer>(Arrays.asList(7,7,0)), 2, 5,5,false,true,2,true, Arrays.asList("",""),Arrays.asList("",""));
+    }
+
+    public int getHpMax() {
+        return HpMax;
+    }
+
+    public int setHpMax(CoeurMax coeurMax) {
+        return this.HpMax ++;
     }
 }
