1695a5d7388a30e2742548aae25b912dbbfd88e2
marcellodestefano <marcello_destefano@hotmail.com>
2025-11-21 11:42:18 +0100

    setPosition debut monstres

---
+++ src/utils/CreationMonstres.java
@@ -0,0 +1,10 @@
+package utils;
+
+import java.util.ArrayList;
+
+public class CreationMonstres {
+    public static ArrayList<Integer> creationMonstres(){
+
+        return new ArrayList<Integer>();
+    }
+}
